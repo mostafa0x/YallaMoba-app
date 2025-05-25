ad403758b50b6b0a0ddc6398a7b140bd810eb06f
mostafa0x <dodomido12350@gmail.com>
2025-05-25 10:17:55 -0700

    feat: add clipboard functionality to profile component for copying user ID

--- app/(tabs)/profile/[uid]/index.tsx
+++ app/(tabs)/profile/[uid]/index.tsx
@@ -14,7 +14,8 @@ import axios from 'axios';
 import { API_BASE_URL } from 'config';
 import { ProfileFace } from 'types/interfaces/store/ProfileFace';
 import PostView from 'components/Post/postView';
-
+import * as Clipboard from "expo-clipboard"
+import callToast from 'components/toast';
 
 export default function Proflie() {
     const { uid } = useLocalSearchParams()
@@ -29,6 +30,20 @@ export default function Proflie() {
         if (uid == userData?.UID) {
             setIsMyProfile(true)
         } else setIsMyProfile(false)
+    }
+
+    async function CopyID(id: string | null) {
+        if (!id) {
+            throw "Error Copy id !"
+        }
+        try {
+
+            await Clipboard.setStringAsync(id)
+            callToast({ type: 'success', text1: "Yalla Moba", text2: "Copy ID Done" })
+        } catch (err: any) {
+            callToast({ type: 'error', text1: "Yalla Moba", text2: err })
+
+        }
     }
 
     async function FatchProfile() {
@@ -65,25 +80,20 @@ export default function Proflie() {
     }
 
     return (
-
         <View style={{ backgroundColor: "#FFFFFF", height: "100%" }}>
             <View className='flex justify-between flex-row border-b-2  border-slate-200'>
                 <View style={{ width: 60 }} className='flex-row items-center text-center pl-4 '>
                     <Text onPress={() => router.back()} className='text-6xl font-normal' >{"<"}</Text>
                     <Icon size={80} source={require('../../../../assets/splash.png')} />
-
                 </View>
-
                 <Text style={{ width: 200 }} className='text-2xl items-center  text-center mt-8'>{userData?.username.toLocaleUpperCase()}</Text>
                 <View className=''>
                     <Text onPress={() => router.push("/menu")} className='text-5xl items-center  text-center'>...!</Text>
                 </View>
-
             </View>
             {/*Posts */}
             <View className=''>
                 <FlatList
-
                     data={ownerPosts}
                     numColumns={3}
                     keyExtractor={(item) => item.id.toString()}
@@ -91,13 +101,9 @@ export default function Proflie() {
                     columnWrapperStyle={{}}
                     style={{ backgroundColor: '#ffffff' }}
                     ListHeaderComponent={<>
-                        {/* top bar */}
-
-
-                        <View style={{ marginBottom: 100 }}>
-
+                        {/* Info Box */}
+                        <View style={{ marginBottom: 100, marginTop: 25 }}>
                             <View className='flex-row items-center justify-around pr-10 '>
-
                                 <Avatar.Image size={115} source={{ uri: userData?.avatar }} />
                                 <View>
                                     <Text style={Style.headerTextTop}>371</Text>
@@ -112,16 +118,13 @@ export default function Proflie() {
                                     <Text style={Style.headerTextBottom}>following.</Text>
                                 </View>
                             </View>
-
                             <View className=''>
                                 <View className='flex-row pl-[185px] gap-2 '>
-
-                                    <Button textColor='white' style={Style.buttonsMain}>Follow</Button>
-                                    <Button textColor='white' style={Style.buttons}>message</Button>
+                                    {isMyProfile ? <Button textColor='white' style={Style.buttonsMain}>Edit Profile</Button> : <Button textColor='white' style={Style.buttonsMain}>Follow</Button>}
+                                    <Avatar.Icon size={45} style={{ backgroundColor: "#ce4500", borderRadius: 20, width: 65, height: 40, marginLeft: 5 }} icon={"send"} />
                                 </View>
                                 <View className='flex-row pl-[185px] gap-2 mt-6 '>
-
-                                    <Button textColor='white' style={Style.buttonsMain}>999432432</Button>
+                                    <Button onPress={() => { CopyID(userData?.UID ?? null) }} textColor='white' style={Style.buttonsMain}>{userData?.UID}</Button>
                                 </View>
                                 {/* Role ICON */}
                                 <View className=' absolute top-[50px] left-[385px]'>
@@ -130,7 +133,6 @@ export default function Proflie() {
 
                             </View>
                         </View>
-
                     </>}
                 />
             </View>
@@ -161,67 +163,10 @@ const Style = StyleSheet.create({
         backgroundColor: "#ce4500"
     }
 })
-
-{/* <View className=' absolute justify-center items-center top-[180px] left-[165px]'>
-                <Image style={{ width: 254, height: 254, objectFit: 'cover', borderRadius: "100%" }} source={require("../../../../assets/gifs/kaguraGif.gif")} />
-                <Text>Loading...</Text>
-            </View> */}
-
-
-
-//          return (<>
-//     <View style={{ backgroundColor: "white" }} className='flex justify-between w-full'>
-//         <View className='flex justify-between flex-row  '>
-//             <Button onPress={() => router.back()} icon="keyboard-return"><Text className='text-black' style={{ fontSize: 28 }}>{ownerData?.username}</Text></Button>
-//             <Button onPress={() => router.push("/menu")} ><Text className='text-black' style={{ fontSize: 28 }}>...</Text></Button>
-//         </View>
-//         <View className='flex justify-around flex-row items-center mt-4'>
-//             <View className=''>
-//                 <View className=' absolute left-[120px]'>
+//   < View className = ' absolute left-[120px]' >
 //                     <Icon
 //                         color={ownerData?.gender == "Male" ? "blue" : "deeppink"}
 //                         source={ownerData?.gender == "Male" ? "gender-male" : "gender-female"}
 //                         size={25}
 //                     />
-//                 </View>
-//                 <View className=' absolute top-28 left-32'>
-
-//                     <IconRole Role={ownerData?.role ?? "Roam"} />
-//                 </View>
-
-//                 <Avatar.Image size={124} source={{ uri: ownerData?.avatar }} />
-
-
-
-
-//             </View>
-//             <View className='text-center'>
-//                 <Text className='text-center'>{userPosts.length}</Text>
-//                 <Text>posts</Text>
-//             </View>
-//             <View className='text-center'>
-//                 <Text className='text-center'>{userFollowers.length}</Text>
-//                 <Text>Followers</Text>
-//             </View>
-//             <View className='text-center'>
-//                 <Text className='text-center'>{userFollowing.length}</Text>
-//                 <Text>Following</Text>
-//             </View>
-//         </View>
-//         {/* <View style={{ backgroundColor: "white" }} className=' absolute top-[175px] left-56'>
-//             <Button style={{ backgroundColor: "lavender", width: 240 }}><Text className='text-black'>Edit Profile</Text></Button>
-//         </View> */}
-
-
-//     </View >
-//     <View style={{ backgroundColor: "white" }} className=' mb-64 mt-[140px]'>
-//         <FlatList
-//             data={ownerPosts}
-//             numColumns={3}
-//             keyExtractor={(item) => item.id.toString()}
-//             renderItem={({ item }) => <PostView post={item} />}
-//             columnWrapperStyle={{}}
-//         />
-
-//     </View>
-// </>
+//                 </>
