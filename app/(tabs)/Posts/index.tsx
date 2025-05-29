499edd30083721d523c900be25b9a0b054b0602d
mostafa0x <dodomido12350@gmail.com>
2025-05-29 11:23:50 -0700

    feat: enhance comments display with user avatars and improved styling; add send button for comment input

--- app/(tabs)/Posts/index.tsx
+++ app/(tabs)/Posts/index.tsx
@@ -1,9 +1,9 @@
-import { View, Text, FlatList, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native'
+import { View, Text, FlatList, StyleSheet, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { StateFace } from 'types/interfaces/store/StateFace'
 import AllPost from 'components/Post/AllPost'
-import { Icon, TextInput } from 'react-native-paper'
+import { Avatar, Button, Icon, TextInput } from 'react-native-paper'
 import { PostFace } from 'types/interfaces/store/ProfileFace'
 import ImageViewing from 'react-native-image-viewing';
 import { useLocalSearchParams } from 'expo-router'
@@ -111,9 +111,16 @@ export default function Post() {
                         data: comments,
                         keyExtractor: (item) => item.id,
                         renderItem: ({ item }) => (
-                            <View >
-                                <Text >{item.user}</Text>
-                                <Text>{item.text}</Text>
+                            <View style={Style.menu}>
+                                <TouchableOpacity className='mb-5'>
+                                    <View className='flex-row items-center gap-2'>
+                                        <Avatar.Image size={30} source={{ uri: 'camera' }} />
+                                        <Text className='text-2xl'>{item.user}.</Text>
+                                    </View>
+                                </TouchableOpacity>
+                                <View className='w-full'>
+                                    <Text className=' pl-5 text-xl'>{item.text}</Text>
+                                </View>
                             </View>
                         ),
                         keyboardShouldPersistTaps: 'handled',
@@ -121,10 +128,13 @@ export default function Post() {
                     }}
                 />
 
-            </KeyboardAvoidingView>
+            </KeyboardAvoidingView >
             {
-                isMenuOpen && <View className='m-2 border-2 p-2 bg-white border-gray-500 justify-end '>
-                    <TextInput placeholder="comment here" />
+                isMenuOpen && <View className=' m-2 border-2 p-2 bg-white items-center border-gray-500 justify-between flex-row '>
+                    <TextInput className='w-[400px]' placeholder="comment here" />
+                    <TouchableOpacity>
+                        <Icon color='blue' size={50} source={'send'} />
+                    </TouchableOpacity>
                 </View>
             }
 
@@ -136,7 +146,11 @@ export default function Post() {
 const Style = StyleSheet.create({
     continer: {
         flex: 1,
-
         backgroundColor: "white"
+    },
+    menu: {
+        flex: 1,
+        margin: 20,
+        marginLeft: 10
     }
 })
